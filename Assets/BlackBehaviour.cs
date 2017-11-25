6ace7bc7846e006853c697391d014686fd3c2305
ca3-saunders <killerchar@hotmail.co.uk>
2017-11-25 01:17:34 +0000

    Made Progress

--- Assets/BlackBehaviour.cs
+++ Assets/BlackBehaviour.cs
@@ -2,18 +2,189 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BlackBehaviour : MonoBehaviour {
-
-	int posX; 
-	int posY; 
-
+ 
+	float speed = 1f; 
+	GameObject[,] gridref; 
+	GameObject prevPos; 
+	Vector3 averageLocationPos = Vector3.zero; 
+	public void init(int X, int Y, GameObject[,] GR)
+	{
+		transform.position = new Vector3 (X, Y);  
+		gridref = GR; 
+		prevPos = new GameObject (); 
+		averageLocationPos = getAverageLocationPos (); 
+	}
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		float changeX = 0; 
+		float changeY = 0; 
+		//speed = Mathf.Round(Random.Range (1, 10)) / 10; 
+		if (transform.localPosition.x > 0.01)
+			changeX -= speed;  
+		if (transform.localPosition.x < -0.01)
+			changeX += speed;  
+		if (transform.localPosition.y > 0.01)
+			changeY -= speed;  
+		if (transform.localPosition.y < -0.01)
+			changeY += speed;  
+		transform.position = new Vector3 (transform.position.x + changeX, transform.position.y + changeY, 0); 
+
+
+		if (changeX == 0 && changeY == 0 && transform.parent.tag != "Fill")
+			switch (Main.Style) 
+			{
+		case Main.style.Search: 
+			move (); 
+			break; 
+			case Main.style.Sweep:  
+			sweep (); 
+			break; 
+			case Main.style.hybrid: 
+			break; 
+			}
+	}
 		
+	void sweep()
+	{
+		int posx = Mathf.RoundToInt (transform.parent.position.x); 
+		int posy = Mathf.RoundToInt (transform.parent.position.y); 
+		//Up
+		if (posy != gridref.GetLength (1) - 1)
+		if (gridref [posx, posy + 1].transform.childCount == 0)
+		{
+			transform.SetParent (gridref [posx, posy + 1].transform);
+			return; 
+		}
+		else if (gridref [posx, posy + 1].transform.childCount == 1 && gridref [posx, posy + 1].tag == "Fill")
+		{
+			transform.SetParent (gridref [posx, posy + 2].transform);
+			return; 
+		}
+		//Left
+		if (posx != 0)
+		if (gridref [posx - 1, posy].transform.childCount == 0)
+		{
+			transform.SetParent (gridref [posx - 1, posy].transform );
+			return; 
+		}
+		//Right
+		if (posx != gridref.GetLength (0) - 1)
+		if (gridref [posx + 1, posy].transform.childCount == 0)
+		{
+			transform.SetParent (gridref [posx + 1, posy].transform);
+			return; 
+		}
+		//Down
+		if (posy != 0)
+		if (gridref [posx, posy - 1].transform.childCount == 0)
+		{
+			transform.SetParent (gridref [posx, posy - 1].transform);
+			return; 
+		}
+	}
+	void find()
+	{
+		//if (!(gridref [posx - 1, posy].transform.childCount == 1 && gridref [posx - 1, posy].tag == "Fill"))
+	}
+
+	void move()
+	{
+		int posx = Mathf.RoundToInt (transform.parent.position.x); 
+		int posy = Mathf.RoundToInt (transform.parent.position.y); 
+		List<System.Action> methodList = new List<System.Action>(); 
+		methodList.Add (() => Left (posx, posy));
+		methodList.Add (() => Right (posx, posy));
+		//methodList.Add (() => Down (posx, posy));
+		methodList.Add (() => Up (posx, posy));
+		methodList.Add (() => Up (posx, posy));
+		methodList.Add (() => Up (posx, posy));
+		methodList.Add (() => Up (posx, posy));
+
+		//prevPos.transform.parent = gridref [posx, posy].transform; 
+		for (int i = 0; i < methodList.Count; i++) 
+		{
+			int randpos = Random.Range (i, methodList.Count);
+			System.Action temp = methodList [i]; 
+			methodList[i] = methodList[randpos];
+			methodList [randpos] = temp; 
+		}
+		for (int i = 0; i < methodList.Count; i++)
+			methodList [i] (); 
+	}
+
+
+	void Left(int posx, int posy)
+	{
+		//Left
+		if (posx != 0)
+		if (gridref [posx - 1, posy].transform.childCount == 0) 
+		{
+			transform.SetParent (gridref [posx - 1, posy].transform);
+			return; 
+		}
+	}
+
+	void Right(int posx, int posy)
+	{
+		//Right
+		if (posx != gridref.GetLength (0) - 1)
+		if (gridref [posx + 1, posy].transform.childCount == 0) 
+		{
+			transform.SetParent (gridref [posx + 1, posy].transform);
+			return; 
+		}
+	}
+
+	void Up(int posx, int posy)
+	{
+		//Up
+		if (posy != gridref.GetLength (1) - 1)
+		{
+			if (gridref [posx, posy + 1].transform.childCount == 0) 
+			{
+				transform.SetParent (gridref [posx, posy + 1].transform);
+				return; 
+			}
+			else if (gridref [posx, posy + 1].transform.childCount == 1 && gridref [posx, posy + 1].tag == "Fill")
+			{
+				if (posy != gridref.GetLength (1) - 2)
+				transform.SetParent (gridref [posx, posy + 2].transform);
+				return; 
+			}
+		}
+	}
+	void Down(int posx, int posy)
+	{
+		//Down
+		if (posy != 0)
+		if (gridref [posx, posy - 1].transform.childCount == 0) 
+		{
+			transform.SetParent (gridref [posx, posy - 1].transform);
+			return; 
+		}
+	}
+	Vector3 getAverageLocationPos()
+	{
+		int num = 0; 
+		Vector3 averagepos = Vector3.zero; 
+		for (int i = 0; i < gridref.GetLength (1); i++) {
+			for (int x = 0; x < gridref.GetLength (0); x++) {
+				if (gridref [x, i].tag == "Fill" && gridref [x, i].transform.childCount == 0) 
+				{
+					averagepos += gridref [x, i].transform.position; 
+					num++; 
+				}
+			}
+		}
+		averageLocationPos = averagepos / num; 
+		return averagepos / num; 
 	}
 }
---
+++ Assets/Main.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+	public enum style
+	{
+		Search, 
+		Sweep, 
+		Find,
+		hybrid
+	};
+	const int row = 100;
+	const int column = 100; 
+	const int numBlacks = 1000; 
+	public GameObject blackTemplate; 
+	GameObject[,] grid = new GameObject[column, row];
+	BlackBehaviour[] blacks = new BlackBehaviour[numBlacks]; 
+	public static style Style = style.Search;  
+	// Use this for initialization
+	void Start () {
+		int count = 0; 
+		for (int i = 0; i < row; i++)
+		{
+		    for (int x = 0; x < column; x++) 
+			{
+				count++; 
+				var go = new GameObject();
+				go.name = "GridSquare : " + x.ToString () + i.ToString (); 
+				go.transform.parent = gameObject.transform; 
+				go.transform.position = new Vector3(x, i, 0); 
+				//go.AddComponent<SpriteRenderer> (); 
+				//SpriteRenderer sr = go.GetComponent<SpriteRenderer> (); 
+				//sr.sprite = Sprite.Create (Texture2D.whiteTexture, new Rect (0f, 0f, 4f, 4f), new Vector2 (0.5f, 0.5f), 100f);
+				if (count % 7 == 0) 
+				{
+					go.tag = "Fill";
+				}
+				grid [x, i] = go; 
+
+			}
+		}
+		for (int i = 0; i < numBlacks; i++) 
+		{
+			int randintx = Random.Range (0, column); 
+			int randinty = Random.Range (0, 0); 
+			GameObject temp = Instantiate (blackTemplate, grid [randintx, randinty].transform) as GameObject;
+			blacks [i] = temp.GetComponent<BlackBehaviour> ();
+			blacks[i].init(randintx, randinty, grid); 
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+//		for (int i = 0; i < blacks.Length; i++) 
+//		{
+//			blacks [i].GetComponentInParent<transform.position> ();
+//		}
+
+	}
+}
